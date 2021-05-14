ea796e88d8fa8261983f8d694d7dab9d82b872cc
Jorge <78565993+badkluster@users.noreply.github.com>
2021-05-14 07:40:37 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/js/sw.js")
-    .then((reg) => console.log("Registro de SW exitoso", reg))
-    .catch((err) => console.warn("Error al tratar de registrar el sw", err));
-}
+
 // MENU RESPONSIVE
 var btnMenuOpen = document.getElementById("btnMenuOpen"),
   btnMenuClose = document.getElementById("btnMenuClose"),
